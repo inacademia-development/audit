fd0e25fcb5638fc49c93b0d1203f0d685254337b
Niels van Dijk <niels.vandijk@surfnet.nl>
2019-05-10 16:35:03 +0200

    Updated docker run statent

--- audit_log_handler_docker/run_loghandler.sh
+++ audit_log_handler_docker/run_loghandler.sh
@@ -5,6 +5,8 @@ source config/audit_handler.cnf
 
 IMAGE_TAG=inacademia/log_handler:v1
 
+export KEY_PATH=$GIT_KEY_PATH
+
 # Build the docker image if needed
 if [[ "$(docker images -q $IMAGE_TAG 2> /dev/null)" == "" ]]; then
   docker build -t $IMAGE_TAG .
@@ -19,12 +21,12 @@ echo "##########################################################################
 if [ ! -d "$REMOTE_DIR" ]; then
 	# If the remote dir does not exists, pull a fresh copy of tha audot logs into the remote dir
 	echo "$REMOTE_DIR not found, pulling new copy of audit logs"
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git clone git@gitlab.geant.org:inacademia/audit_logs.git $REMOTE_DIR
+	GIT_SSH_COMMAND='ssh -i ${KEY_PATH}' /usr/bin/git clone git@gitlab.geant.org:inacademia/audit_logs.git $REMOTE_DIR
 else
 	# Remote dir exists, so cd into the remote dir and pull the lates version of the logspull a fresh copy of tha audot logs into the remote dir
 	echo "$REMOTE_DIR found, updating audit logs"
 	cd $REMOTE_DIR
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git pull git@gitlab.geant.org:inacademia/audit_logs.git
+	GIT_SSH_COMMAND='ssh -i "$KEY_PATH"' /usr/bin/git pull git@gitlab.geant.org:inacademia/audit_logs.git
 fi
 
 # Pull in admin data from git@gitlab.geant.org:inacademia/admin_data.git
@@ -37,12 +39,12 @@ echo "##########################################################################
 if [ ! -d "$ADMIN_DATA_DIR" ]; then
 	# If the remote dir does not exists, pull a fresh copy of tha audot logs into the remote dir
 	echo "$ADMIN_DATA_DIR not found, pulling new copy of admin data logs"
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git clone git@gitlab.geant.org:inacademia/admin_data.git $ADMIN_DATA_DIR
+	GIT_SSH_COMMAND='ssh -i "$KEY_PATH"' /usr/bin/git clone git@gitlab.geant.org:inacademia/admin_data.git $ADMIN_DATA_DIR
 else
 	# Remote dir exists, so cd into the remote dir and pull the lates version of the logspull a fresh copy of tha audot logs into the remote dir
 	echo "$ADMIN_DATA_DIR found, updating audit logs"
 	cd $ADMIN_DATA_DIR
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git pull git@gitlab.geant.org:inacademia/admin_data.git
+	GIT_SSH_COMMAND='ssh -i "$KEY_PATH"' /usr/bin/git pull git@gitlab.geant.org:inacademia/admin_data.git
 fi
 
 # Pull in idphint data from git@gitlab.geant.org:inacademia/admin_data.git
@@ -55,17 +57,17 @@ echo "##########################################################################
 if [ ! -d "$IDP_HINT_DIR" ]; then
 	# If the remote dir does not exists, pull a fresh copy of tha audot logs into the remote dir
 	echo "$IDP_HINT_DIR not found, pulling new copy of idp hint data"
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git clone https://github.com/InAcademia/idp_hint.git $IDP_HINT_DIR
+	GIT_SSH_COMMAND='ssh -i "$KEY_PATH"' /usr/bin/git clone https://github.com/InAcademia/idp_hint.git $IDP_HINT_DIR
 else
 	# Remote dir exists, so cd into the remote dir and pull the lates version of the logspull a fresh copy of tha audot logs into the remote dir
 	echo "$IDP_HINT_DIR found, updating idp hint data"
 	cd $IDP_HINT_DIR
-	GIT_SSH_COMMAND='ssh -i /home/niels/dev/InAcademia/inacademia-audit/audit_log_handler_docker/config/id_rsa_inacademia' /usr/bin/git pull https://github.com/InAcademia/idp_hint.git
+	GIT_SSH_COMMAND='ssh -i "$KEY_PATH"' /usr/bin/git pull https://github.com/InAcademia/idp_hint.git
 fi
 
 cp $IDP_HINT_DIR/display_names.json $ADMIN_DATA_DIR/display_names.json
 
-# Start SVS
+# Start Docker
 docker run -it \
 	--ip $LOGHANDLER_HOST_IP \
 	--net inacademia.local \
@@ -74,12 +76,12 @@ docker run -it \
 	-v /home/ubuntu:/home/ubuntu \
 	-v $REMOTE_DIR:/tmp/remote:ro \
 	-v $ADMIN_DATA_DIR:/tmp/inacademia_admin_data:ro \
-    -v $CDB_DIR:/tmp/cdb:ro \
+        -v $CDB_DIR:/tmp/cdb:ro \
 	-v $RUN_DIR/app:/tmp/inacademia \
-    -e MYSQL_ROOT_PASSWORD=$MYSQL_PWD \
-    -e MYSQL_DATABASE=$MYSQL_DB \
-    -e MYSQL_USER=$MYSQL_USER \
-    -e MYSQL_HOST=$MYSQL_HOST \
+        -e MYSQL_ROOT_PASSWORD=$MYSQL_PWD \
+        -e MYSQL_DATABASE=$MYSQL_DB \
+        -e MYSQL_USER=$MYSQL_USER \
+        -e MYSQL_HOST=$MYSQL_HOST \
 	--hostname log_handler.inacademia.local \
 	$IMAGE_TAG
 
