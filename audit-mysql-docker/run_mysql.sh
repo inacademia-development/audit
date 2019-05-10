99ad17300832b5ff74a64613ad50a609c5fe1c21
Niels van Dijk <niels.vandijk@surfnet.nl>
2019-05-10 16:33:37 +0200

    Added auto creation of MySQL databases

--- audit-mysql-docker/run_mysql.sh
+++ audit-mysql-docker/run_mysql.sh
@@ -9,18 +9,19 @@ CONTAINER_NAME=inacademia_mysqllogs
 
 # Start MySQL image
 docker start -i $CONTAINER_NAME || docker run -it \
-     --default-authentication-plugin=mysql_native_password
+     --default-authentication-plugin=mysql_native_password \
      --net inacademia.local \
      --ip $MYSQL_IP \
      -e MYSQL_ROOT_PASSWORD=$MYSQL_PWD \
      -e MYSQL_DATABASE=$MYSQL_DB \
      -e MYSQL_USER=$MYSQL_USER \
      -e MYSQL_PASSWORD=$MYSQL_PWD \
+     -v "$DIR"/config/sql:/docker-entrypoint-initdb.d \
      -d \
      $IMAGE_TAG
 
 
-
+# COPY setup.sql /docker-entrypoint-initdb.d/
 #      -p 3306:3306 \
 #     -v $DIR/mysql_data_dir:/var/lib/mysql \
 #     --name $CONTAINER_NAME \
